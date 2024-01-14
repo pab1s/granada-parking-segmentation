f5036cd99a0bbad6aa4e59fd1353d7ea60f69272
Pablo Olivares <pablolivares1502@gmail.com>
2024-01-14 22:16:35 +0100

    Delete unused images and update loss graphics

--- src/evaluate/loss_graphics.py
+++ src/evaluate/loss_graphics.py
@@ -1,18 +1,32 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
 
-train_loss = [0.092188, 0.085606, 0.082553, 0.082288, 0.080601, 0.079573, 0.077640, 0.076990, 0.076229, 0.074265]
-valid_loss = [0.112501, 0.085238, 0.068926, 0.066756, 0.077916, 0.083918, 0.066423, 0.063398, 0.061721, 0.060911]
+csv_paths = [
+    'results/logs/pspnet_metrics.csv',
+    'results/logs/deeplab_metrics.csv',
+    'results/logs/unet_metrics.csv'
+]
+model_names = ['PSPNet', 'DeepLabV3+', 'Dynamic UNet']
 
 sns.set_style("darkgrid")
+colors = ['b', 'g', 'r'] 
 
-sns.lineplot(x=range(len(train_loss)), y=train_loss, label='Train Loss')
-sns.lineplot(x=range(len(valid_loss)), y=valid_loss, label='Valid Loss')
+for csv_file, model_name, color in zip(csv_paths, model_names, colors):
+    data = pd.read_csv(csv_file)
+
+    train_loss = data['train_loss']
+    valid_loss = data['valid_loss']
+
+    sns.lineplot(x=range(len(train_loss)), y=train_loss, label=f'{model_name} Train Loss', color=color)
+    sns.lineplot(x=range(len(valid_loss)), y=valid_loss, label=f'{model_name} Valid Loss', linestyle='--', color=color)
 
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
-#plt.title('Training and Validation Loss Over Epochs')
+# plt.title('Training and Validation Loss Over Epochs for Different Models')
 plt.legend()
 
-plt.savefig('loss_plot.png')
+plt.ylim(0, 0.5)
+
+plt.savefig('train_valid_loss_plot.png')
 plt.show()
